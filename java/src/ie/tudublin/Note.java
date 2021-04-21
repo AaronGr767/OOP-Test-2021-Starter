c914c62fe58aa60736271f22c58295eb25641596
AaronGr767 <aarongroarke2@gmail.com>
2021-04-21 19:17:12 +0100

    added setters and getters to Note class aswell as adding paramters to method

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -5,9 +5,25 @@ public class Note{
 	private char note;
 	private int duration;
 
-	public Note(){
+	public Note(char note, int duration){
 		this.note = note;
 		this.duration = duration;	
+	}
+
+	public char getNote(){
+		return note;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setNote(char note){
+		this.note = note;
+	}
+
+	public void setDuration(int duration){
+		this.duration = duration;
 	}
 
 	public String toString(){
